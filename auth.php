70bb1855ddb1a54ba3710e8383490c7c4648a070
sheikh hussnain <sheikhhussnain23@gmail.com>
2018-06-04 09:01:03 +0500

    Scope issue Fixed

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@
 $file = file_get_contents('./client_secrets.json'); //Client Secrets file
 $data = json_decode($file);
 $client_id = $data->web->client_id;
-$scope = "https://www.googleapis.com/auth/userinfo.email&https://www.google.com/m8/feeds";
+$scope = "https://www.googleapis.com/auth/userinfo.email https://www.google.com/m8/feeds";
 $redirect_uri = $data->web->redirect_uris[0];
 
 $url = "https://accounts.google.com/o/oauth2/v2/auth?scope={$scope}&response_type=code&state=security_token&redirect_uri={$redirect_uri}&client_id={$client_id}&access_type=offline";
